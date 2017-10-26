ab1a391a0feea260930bc74bb6a0d139bb15e04b
Your Name <you@example.com>
2017-10-26 16:46:07 +0200

    last giy

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
-   // Lift
-
-
-// this the first mission
+   
+// this will build new array
 $building= BuildArray(10);
-// this the second mission
+Printing(10);
+// this wil set the lifts
 $lifts= SetLifts(4);
 Printing(10);
-
+//this will call the nearest lift to the user
+$neararray=array();
 $user= 7;
+$near=100;
+CallLift();
+SendLift($neararray,$user);
 
 
 function BuildArray($numFloor)
@@ -19,19 +22,18 @@ function BuildArray($numFloor)
     {
         
         $arr[$x]=0;
-        echo "$arr[$x]";
-        echo "<br>";
-        
+               
     }
     return $arr;
 }
 
-        
+    
 function SetLifts($numLift)
 {
     
    for($x=0;$x < $numLift;$x++) 
    {
+       global $building;
        $building[$x]=1;
    }
     
@@ -41,17 +43,57 @@ function SetLifts($numLift)
         
 function Printing($numFloor)
 {
-    
+   
    for($x=0;$x < $numFloor;$x++) 
    {
-       echo $building[$x];
+       global $building;
+       echo "$x". " ==> ".$building[$x];
+       echo "<br>";
+             
    }
     
-   
+   echo "<hr>";
    
 }         
         
+function CallLift()
+{
+    global $building;
+    global $neararray;
+    $y=0;
+    
+    for($x=0;$x <count($building);$x++)
+    {
+        if($building[$x] == 1)
+        {
+            echo "there is a lift in floor ".$x. "<br>";
+            $neararray[$y]= $x;
+            $y++;
+        }
         
-        
- ?>
-   
+    }
+           
+} 
+
+function SendLift($neararray,$user)
+{
+    global $neararray;
+    global $near;
+    $dif=0;
+    $Hello=0;
+    
+    
+    for($x=0;$x <count($neararray);$x++)
+    {
+       
+        $dif= $user - $neararray[$x];
+                       
+        if(abs($dif) < $near )
+        {
+            $near=$dif;
+            $Hello=$neararray[$x];            
+        }
+    }
+   echo "The nearest lift is in floor ".$Hello;
+}
+?>
